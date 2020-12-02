079b9c6ed0af41f4ff5944de1851ff0f7e6649fb
Andrei-Horia Pacurar <67952156+Andrei-Horia@users.noreply.github.com>
2020-12-02 08:52:36 +0200

    Save the file as "Testing"

--- Videocapture.py
+++ Videocapture.py
@@ -4,12 +4,16 @@ import ClassImageProcessing
 import random
 import time
 
+
+
+###------------Record-------------####
 def record():
     cap = cv2.VideoCapture(0)
 
     imageChecker = False
     count = 0
 
+    #Load Loading Screen
     loadingScreen = cv2.imread("L.jpg")
     loadingScreen = cv2.resize(loadingScreen, (640,482), interpolation = cv2.INTER_AREA)
 
@@ -28,14 +32,16 @@ def record():
             cv2.rectangle(gray,(450,150),(620,350),(255,255,255),3)
         else:
             cv2.rectangle(gray,(450,150),(620,350),(0,255,0),3)
-        
+
+        #Draw the box
         cv2.line(gray,(535,150),(535,350),(130,130,130),2)
         cv2.line(gray,(450,250),(620,250),(130,130,130),2)
 
+        #Display the letter
         if(letter != "" and letter != " "):
             cv2.putText(gray,str(letter), (120,360), cv2.FONT_HERSHEY_SIMPLEX, 3, (255,255,255),10)
+
         # Display the resulting frame
-        
         if lock<=7:
             cv2.imshow('frame',loadingScreen)
         else:
@@ -43,18 +49,21 @@ def record():
 
         count += 1
         lock += 1
-        
+
+        #Save the frame
         if((count-10)%100==0 and count >= 100):
             cv2.imwrite("test.jpg", frame)
             lock = 0
 
+        #Analyze the frame
         if(lock == 7):
             A = ClassImageProcessing.ImageProcessing()
             imageChecker = A.startAll()
             val = A.getMessage()
             letter = val[-1]
-            print(val)
+            #print(val)
 
+        #Check the pressed key
         pressedKey = cv2.waitKey(1) & 0xFF
             
         if pressedKey == ord('q'):
@@ -70,11 +79,11 @@ def record():
     cap.release()
     #Photoes.init(200)
     cv2.destroyAllWindows()
-    val = A.exit()
+    val = A.getMessage()
     A.reset()
     return val
     
-
+###---------------------Game Record------------------###
 def gameRecord():
 
     cap = cv2.VideoCapture(0)
@@ -84,6 +93,7 @@ def gameRecord():
     val = None
     gameRound = 0
 
+    #Add the loading Screen
     loadingScreen = cv2.imread("L.jpg")
     loadingScreen = cv2.resize(loadingScreen, (640,482), interpolation = cv2.INTER_AREA)
 
@@ -106,6 +116,7 @@ def gameRecord():
         elif(val == True and count < 20):
             cv2.rectangle(gray,(450,150),(620,350),(255,255,255),3)
 
+        #Format the box
         cv2.line(gray,(535,150),(535,350),(130,130,130),2)
         cv2.line(gray,(450,250),(620,250),(130,130,130),2)
 
@@ -118,7 +129,8 @@ def gameRecord():
             cv2.imshow('frame',loadingScreen)
 
         lock += 1
-        
+
+        #Analyze the scores
         if(OK == True and lock == 7):
             A = ClassImageProcessing.Game(letter)
             val = A.start()
@@ -129,7 +141,8 @@ def gameRecord():
             letter = random.randint(0,25) + 65
             gameRound += 1
             OK = False
-            
+
+        #Remeber which key was pressed
         pressedKey = cv2.waitKey(1) & 0xFF
             
         if pressedKey == ord('q'):
